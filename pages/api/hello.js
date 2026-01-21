2fc4b90decfd9c62082ea6e28a8ab2c8f1dbd06d
MarcoFco82 <46635344+MarcoFco82@users.noreply.github.com>
2026-01-21 04:01:50 -0600

    api route api hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export const runtime = 'edge';
+
+export default function handler(req) {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
